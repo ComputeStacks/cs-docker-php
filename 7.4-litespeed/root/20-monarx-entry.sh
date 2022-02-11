76bd0de6cf580134fdcffd267b8c68c5a281ef8d
Kris Watson <kris@computestacks.com>
2022-02-11 12:48:25 -0800

    Support for modifying monarx agent tags

--- 7.4-litespeed/root/20-monarx-entry.sh
+++ 7.4-litespeed/root/20-monarx-entry.sh
@@ -5,6 +5,12 @@ if [ -z ${MONARX_ID} ] || [ -z ${MONARX_SECRET} ]; then
 else
   sed -i "s/SET_CLIENT_ID/$MONARX_ID/g" /etc/monarx-agent.conf
   sed -i "s/SET_CLIENT_SECRET/$MONARX_SECRET/g" /etc/monarx-agent.conf
+  # Fallback to hostname if agent is not set.
+  if [ -z ${MONARX_AGENT} ]; then    
+    sed -i "s/SET_SERVICE_NAME/$HOSTNAME/g" /etc/monarx-agent.conf
+  else
+    sed -i "s/SET_SERVICE_NAME/$MONARX_AGENT/g" /etc/monarx-agent.conf
+  fi
   # grab latest file
   if [ -f /usr/local/lsws/lsphp74/lib/php/20190902/monarxprotect-php74.so ]; then
     rm /usr/local/lsws/lsphp74/lib/php/20190902/monarxprotect-php74.so
