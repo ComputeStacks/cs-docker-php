b1a17418b48e7ae675356a54d8dc02a7d58c3438
Douwe Zijlstra <douwe@xxlhosting.nl>
2022-06-23 14:24:38 +0200

    8.1 and 8.0 versions tested and done

---
+++ 8.1-litespeed/root/40-ls-config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if ! [ "$(ls -A /usr/local/lsws)" ]; then
+  echo >&2 "OpenLiteSpeed not configured, setting configuration..."
+  
+  if [ -z ${PHP_LSAPI_CHILDREN} ]; then
+    echo >&2 "SET_PHP_CHILDREN not set, setting default of 35."
+    sed -i "s/SET_PHP_CHILDREN/35/g" /usr/src/lsws/conf/httpd_config.conf
+  else
+    sed -i "s/SET_PHP_CHILDREN/$PHP_LSAPI_CHILDREN/g" /usr/src/lsws/conf/httpd_config.conf
+  fi
+  
+  if [ -z ${PHP_MAX_CONN} ]; then
+    echo >&2 "PHP_MAX_CONN not set, setting default of 35."
+    sed -i "s/SET_PHP_MAX_CONN/35/g" /usr/src/lsws/conf/httpd_config.conf
+  else
+    sed -i "s/SET_PHP_MAX_CONN/$PHP_MAX_CONN/g" /usr/src/lsws/conf/httpd_config.conf
+  fi  
+  
+fi
