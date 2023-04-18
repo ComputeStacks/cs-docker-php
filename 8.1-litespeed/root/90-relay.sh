5f33a150c520d1532150ccbf5427f83e7deaeb68
Douwe Zijlstra <douwe@xxlhosting.nl>
2023-04-18 12:48:23 +0200

    Relay setup in PHP 8.1

--- 8.1-litespeed/root/90-relay.sh
+++ 8.1-litespeed/root/90-relay.sh
@@ -1,10 +1,54 @@
 #!/bin/bash
 
+# Installation of Relay
 RELAY=v0.6.1
 PLATFORM=$(uname -m | sed 's/_/-/')
+RELAY_PHP=$(/usr/local/lsws/lsphp81/bin/php-config --version | cut -c -3)  # 8.1
+RELAY_INI_DIR=$(/usr/local/lsws/lsphp81/bin/php-config --ini-dir)          # /etc/php/8.1/cli/conf.d/
+RELAY_EXT_DIR=$(/usr/local/lsws/lsphp81/bin/php-config --extension-dir)    # /usr/lib/php/20210902
 
-curl -L "https://builds.r2.relay.so/$RELAY/relay-$RELAY-php8.1-debian-$PLATFORM%2Blibssl3.tar.gz" | tar xz -C /tmp
-cp "/tmp/relay-$RELAY-php8.1-debian-$PLATFORM+libssl3/relay.ini" "$PHP_INI_DIR/60-relay.ini" 
-cp "/tmp/relay-$RELAY-php8.1-debian-$PLATFORM+libssl3/relay-pkg.so" "$PHP_EXT_DIR/relay.so"
+# if $PHP_INI_DIR/60-relay.ini does not exist, cp relay.ini to $PHP_INI_DIR/60-relay.ini
+if [ ! -f "$RELAY_INI_DIR/60-relay.ini" ]; then
+    cp "/usr/src/relay/relay-$RELAY-php$RELAY_PHP-debian-$PLATFORM+libssl3/relay.ini" "$RELAY_INI_DIR/60-relay.ini"
+fi
+
+if [ ! -f "$RELAY_EXT_DIR/relay.so" ]; then
+    cp "/usr/src/relay/relay-$RELAY-php$RELAY_PHP-debian-$PLATFORM+libssl3/relay-pkg.so" "$RELAY_EXT_DIR/relay.so"
+fi
+
+cp "/usr/src/relay/relay-$RELAY-php8.1-debian-$PLATFORM+libssl3/relay.ini" "$PHP_INI_DIR/60-relay.ini" 
+cp "/usr/src/relay/relay-$RELAY-php8.1-debian-$PLATFORM+libssl3/relay-pkg.so" "$PHP_EXT_DIR/relay.so"
 
 sed -i "s/00000000-0000-0000-0000-000000000000/$(cat /proc/sys/kernel/random/uuid)/" "$PHP_EXT_DIR/relay.so"
+
+# setting configuration;
+RELAY_INI="/usr/local/lsws/lsphp81/etc/php/8.1/litespeed/php.ini"
+
+# if RELAY_MEMORY is not defined, set to default value of 128m
+if [ -z "$RELAY_MEMORY" ]; then
+    RELAY_MEMORY=128M
+fi
+
+if [ -z "$RELAY_LICENSE" ]; then
+    sed -i "s/^;\? \?relay.key =.*/relay.key = $RELAY_LICENSE/" $RELAY_INI
+    sed -i "s/^;\? \?relay.maxmemory =.*/relay.maxmemory = ${RELAY_MEMORY:-128M}/" $RELAY_INI
+else
+    sed -i "s/^;\? \?relay.maxmemory =.*/relay.maxmemory = 32M/" $RELAY_INI
+fi
+
+
+sed -i "s/^;\? \?relay.environment =.*/relay.environment = ${RELAY_ENV:-production}/" $RELAY_INI
+sed -i "s/^;\? \?relay.maxmemory_pct =.*/relay.maxmemory_pct = ${RELAY_MEMORY_PCT:-75}/" $RELAY_INI
+sed -i "s/^;\? \?relay.eviction_policy =.*/relay.eviction_policy = ${RELAY_EVICTION_POLICY:-noeviction}/" $RELAY_INI
+sed -i "s/^;\? \?relay.eviction_sample_keys =.*/relay.eviction_sample_keys = ${RELAY_EVICTION_SAMPLE_KEYS:-128}/" $RELAY_INI
+sed -i "s/^;\? \?relay.default_pconnect =.*/relay.default_pconnect = ${RELAY_DEFAULT_PCONNECT:-1}/" $RELAY_INI
+sed -i "s/^;\? \?relay.databases =.*/relay.databases = ${RELAY_DATABASES:-16}/" $RELAY_INI
+sed -i "s/^;\? \?relay.max_endpoint_dbs =.*/relay.max_endpoint_dbs = ${RELAY_MAX_ENDPOINT_DBS:-32}/" $RELAY_INI
+sed -i "s/^;\? \?relay.initial_readers =.*/relay.initial_readers = ${RELAY_INITIAL_READERS:-128}/" $RELAY_INI
+sed -i "s/^;\? \?relay.invalidation_poll_freq =.*/relay.invalidation_poll_freq = ${RELAY_INVALIDATION_POLL_FREQ:-5}/" $RELAY_INI
+sed -i "s/^;\? \?relay.loglevel =.*/relay.loglevel = ${RELAY_LOGLEVEL:-off}/" $RELAY_INI
+sed -i "s|^;\? \?relay.logfile = .*|relay.logfile = ${RELAY_LOGFILE:-/var/log/www/logs/relay}|" $RELAY_INI
+
+if $RELAY_ENABLED != "true"; then
+    sed -i "s/^;\? \?relay.maxmemory =.*/relay.maxmemory = 0/" $RELAY_INI
+fi
